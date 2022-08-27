08f8dc9671b63888f85e73483190f17057237cb9
ShadePrima <aleksandrag1984@gmail.com>
2022-08-27 19:46:20 +0300

    add global folder

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Text>This is my third project!</Text>
       <StatusBar style="auto" />
     </View>
